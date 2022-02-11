4fe874eb38a13a0980a28dd93afc5422b616f348
rapzaDev <rapzadev@gmail.com>
2022-02-11 11:26:05 -0300

    Creation of Global Styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './global/styles/global.scss';
+
 import App from './App';
 
 ReactDOM.render(
@@ -10,3 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+ 
