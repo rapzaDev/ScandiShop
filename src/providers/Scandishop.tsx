971b66d7d709bb4b5ed1ea7ec959f5e88d2ceda0
rapzaDev <rapzadev@gmail.com>
2022-03-22 19:14:19 -0300

    Removed the manually manipulation of the localStorage to avoid vulnerability issues and installed the redux-persist lib to solve this issue.

--- src/providers/Scandishop.tsx
+++ src/providers/Scandishop.tsx
@@ -1,17 +1,20 @@
 import { ApolloProvider } from '@apollo/client';
 import React, { PureComponent } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { Router as Routes } from '../routes';
 import { client } from '../services/apollo/client';
-import store from '../services/redux/store';
+import STORE from '../services/redux/store';
 
 class ScandiShop extends PureComponent {
   render() {
     return (
       <ApolloProvider client={client}>
-        <ReduxProvider store={store}>
-          <Routes />
+        <ReduxProvider store={STORE.store}>
+          <PersistGate loading={null} persistor={STORE.persistor}>
+            <Routes />
+          </PersistGate>
         </ReduxProvider>
       </ApolloProvider>
     );
--- src/services/redux/store/index.ts
+++ src/services/redux/store/index.ts
@@ -1,4 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import CartProductsContext from '../contexts/CartProducts';
 import CategoryContext from '../contexts/Categories';
@@ -8,18 +10,32 @@ import CurrencyOptionsContext from '../contexts/CurrencyOptions';
 import MyBagContext from '../contexts/MyBag';
 import TextAttributesContext from '../contexts/TextAttributes';
 
+const persistedConfig = {
+  key: '@scandishop/cartProducts',
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistedConfig,
+  CartProductsContext.reducer
+);
+
 const store = configureStore({
   reducer: {
     myBag: MyBagContext.reducer,
     currencyOptions: CurrencyOptionsContext.reducer,
     categories: CategoryContext.reducer,
     currencies: CurrenciesContext.reducer,
-    products: CartProductsContext.reducer,
+    products: persistedReducer,
     textAttributes: TextAttributesContext.reducer,
     colorAttributes: ColorAttributesContext.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export default { store, persistor };
